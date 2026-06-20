3191b6bc7ef4a9b10e68e4fe98ce95ec482bd8af
 <anaa_alvarad@hotmail.com>
2026-06-19 20:59:27 -0500

    Contenido visto en la clase, y material de apoyo

---
+++ Clase_2_19_Junio/app.js
@@ -0,0 +1,79 @@
+//! Esto es un comentario
+//! Lo omite el programa cuando se ejecuta 
+//! es info para mi 
+
+//* Variables: 
+//* Sintaxis --> Reglas del lenguaje
+let tituloTarea = "Comprar café";
+
+const fechaCreacion = "2026-06-19"
+let precio = 5;
+const pi = 3.1417;
+console.log(tituloTarea);
+console.log(fechaCreacion);
+
+tituloTarea = "Comprar café Y PAN ";
+console.log(tituloTarea);
+
+// fechaCreacion = "2026-06-18"
+// console.log(fechaCreacion);
+
+
+//! Tipos de datos: 
+
+//& String 
+let mensaje = "Soy un mensaje"; 
+
+//& number
+let unNumero = 4;
+let otroNumero = 4.5;
+
+//& boolean 
+let esValido = false;
+
+//& Undefined vs Null
+let descripcion ;
+let descripcionNull = null;
+
+console.log(descripcion);
+console.log(descripcionNull);
+
+
+//! Operadores 
+console.log("Acá empiezan los operadores:");
+
+//* Operadores aritmeticos
+//* + - * / 
+let num1 = 5;
+let num2 = 5;
+
+console.log("Suma: ", (num1 + num2));
+console.log("Resta: ", (num1 - num2));
+console.log("Multiplicación: ", (num1 * num2));
+console.log("División: ", (num1 / num2));
+
+
+//& Comparación: 
+//& Comparación: 
+console.log("Acá empiezan los operadores arimeticos:");
+
+const prioridadA = 5;
+const prioridadB = 3;
+
+console.log(prioridadA > prioridadB);  
+console.log(prioridadA < prioridadB);  
+console.log(prioridadA === prioridadB); 
+//console.log(prioridadA == prioridadB); 
+
+//& Lógicos: 
+console.log("Acá empiezan los operadores logicos:");
+
+const prioridadAlta = true;
+const completada = false;
+
+console.log(prioridadAlta && !completada); 
+console.log(prioridadAlta || !completada); 
+
+
+
+
---
+++ Clase_2_19_Junio/conversionTipos.js
@@ -0,0 +1,27 @@
+// const precioTexto = "5";
+// //const precioNumero = Number(precioTexto);
+// const envio = 10;
+
+// //console.log(precioNumero + envio);
+// console.log(precioTexto + envio);
+
+
+// const cantidad = 3;
+// const mensaje = "Tienes " + String(cantidad) + " tareas";
+
+// console.log(mensaje);
+
+
+
+
+
+
+const precioProducto = "5";
+const precioEnvio = 10;
+const total = precioProducto + precioEnvio;
+
+console.log(total); 
+
+console.log("5" + 2);
+console.log("5" - 2);
+console.log("5" * 2);
